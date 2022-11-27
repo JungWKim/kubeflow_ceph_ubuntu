960b03a168ca2f250e51991acabb1cd298d35adf
JungWKim <lionmini@naver.com>
2022-11-28 07:02:31 +0900

    .

--- setup_ceph_cluster.sh
+++ setup_ceph_cluster.sh
@@ -11,6 +11,9 @@ echo rbd | tee -a /etc/modules
 git clone --single-branch --branch v1.10.6 https://github.com/rook/rook.git
 
 #------------- install rook operator by helm
+#kubectl apply -f rook/deploy/examples/crds.yaml
+#kubectl apply -f rook/deploy/examples/common.yaml
+#kubectl apply -f rook/deploy/examples/operator.yaml
 sed -i "s/enableDiscovertyDaemon\: false/enableDiscovertyDaemon\: true/" rook/deploy/charts/rook-ceph/values.yaml
 sed -i "s/tag\: VERSION/tag\: v1.10.6/" rook/deploy/charts/rook-ceph/values.yaml
 helm repo add rook-release https://charts.rook.io/release
@@ -18,7 +21,10 @@ helm install --create-namespace --namespace rook-ceph rook-ceph rook-release/roo
 sleep 5
 
 #------------- create ceph cluster
-kubectl apply -f rook/deploy/examples/cluster.yaml
+#kubectl apply -f rook/deploy/examples/cluster.yaml
+sed -i "s/image\: rook\/ceph\:VERSION/image\: rook\/ceph\:v1.10.6/" rook/deploy/charts/rook-ceph-cluster/values.yaml
+helm install --create-namespace --namespace rook-ceph rook-ceph-cluster \
+   --set operatorNamespace=rook-ceph rook-release/rook-ceph-cluster -f rook/deploy/charts/rook-ceph-cluster/values.yaml
 sleep 5
 
 #------------- install rook toolbox
